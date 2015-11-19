1c299a9efa1df738af8631fbe323924962666563
ljl <ljl@BMImatoMac-mini.local>
2015-11-19 16:33:44 +0800

    11.19

--- osMake.py
+++
@@ -1,6 +0,0 @@
-#!user/bin/env python 
-
-import os
-
-os.mkdir('images')
-os.mkdir('images\\image2')
--- reque.py
+++
@@ -1,9 +0,0 @@
-
-#!user/bin/env python 
-#coding:utf-8
-
-import requests
-
-r = requests.get('http://www.baidu.com/link?url=QeTRFOS7TuUQRppa0wlTJJr6FfIYI1DJprJukx4Qy0XnsDO_s9baoO8u1wvjxgqN', allow_redirects = False)
-print r
-print r.status_code
